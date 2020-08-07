2a5f74644a0d6dccefeedf621b7f6bd89ae9747e
Ivan Duran <idurandiaz@gmail.com>
2020-08-07 07:03:03 +0300

    Render the posts in the blog's page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,43 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
 import Layout from "../components/layout"
 
 const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+            excerpt
+          }
+        }
+      }
+    }
+  `)
+
+  const { edges: posts } = data.allMarkdownRemark
+
   return (
     <div>
       <Layout>
-        <h1>Blog</h1>
-        <p>Posts will show up here later on.</p>
+        <h1>List of Blogs</h1>
+        <ol>
+          {posts.map(post => {
+            const { frontmatter, excerpt } = post.node
+            return (
+              <li key={frontmatter.title}>
+                <h2>{frontmatter.title}</h2>
+                <h4>Date: {frontmatter.date}</h4>
+                <p>{excerpt}</p>
+              </li>
+            )
+          })}
+        </ol>
       </Layout>
     </div>
   )
