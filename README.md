68463d904ee7ba9a71f6f1bfdc9e45e804e0da56
Ivan Duran <idurandiaz@gmail.com>
2020-08-10 15:50:03 +0300

    Update README with netlify link and index page

--- README.md
+++ README.md
@@ -9,3 +9,7 @@
 </h1>
 
 This is a simple blog app created using Gatsby, based on the tutorial by Andrew Mead ("[The Great Gatsby Bootcamp](https://youtu.be/8t0vNu2fCCM)").
+
+The blog fetches posts from both the file system (markdown files) and from Contentful CMS.
+
+The app is deployed to Netlify [here](https://reverent-mayer-92054b.netlify.app/)
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,8 +11,7 @@ const IndexPage = () => {
         <Head title="Home" />
         <h1>Hello.</h1>
         <h2>
-          I'm Ivan, a full-stack developer living in Saudi Arabia. More{" "}
-          <Link to="/about">here.</Link>
+          I'm Ivan, a full-stack developer. More <Link to="/about">here.</Link>
         </h2>
         <p>
           Need a developer? <Link to="/contact">Contact me.</Link>
